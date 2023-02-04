f44fe3f6e3e6023a4034a54c259714d6b36bdead
Hasan Huda <hasanhuda037@gmail.com>
2023-02-03 23:06:53 -0500

    first commit

---
+++ PuzzleJava.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+public class PuzzleJava{
+  
+  Random randMachine = new Random();
+  // From there you can use any of the methods listed in the documentation. For example:
+  randMachine.setSeed(35679); // <--- you won't need to use this method.
+
+
+}
